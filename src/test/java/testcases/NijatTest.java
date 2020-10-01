c1e408a051bcef5e368d4d479d0836933dfcc316
Nijat Muhtar <yasunaz1990@gmail.com>
2020-09-30 20:41:44 -0400

    Automated TC5, TC6

---
+++ src/test/java/testcases/NijatTest.java
@@ -0,0 +1,18 @@
+package testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NijatTest {
+
+    @Test
+    public void test_case_five() {
+        Assert.assertEquals(10,11);
+        // error fix code here
+    }
+
+    @Test
+    public void test_case_six() {
+        Assert.assertEquals(10,11);
+    }
+}
